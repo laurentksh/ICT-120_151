0b9a7ec2e9cd6e0ba31c32fff2664022bb523afc
Laurent Keusch <25180858+laurentksh@users.noreply.github.com>
2021-01-07 14:33:12 +0100

    Init

---
+++ src/back/ICT-151/ICT-151/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICT_151.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("auth")]
+        public async Task Authenticate(string email, string password) //TODO
+        {
+
+        }
+    }
+}
